d2e5a51f069349378bf1a0a5c6d96e22bd8f821f
talucre <talucreee@gmail.com>
2026-04-17 19:40:54 +0300

    feature: added icons

--- src/common/components/Navbar/Navbar.tsx
+++ src/common/components/Navbar/Navbar.tsx
@@ -1,7 +1,16 @@
-import s from './Navbar.module.scss'
 import { NavLink } from 'react-router'
 import { Path } from '@/common/routing'
+
 import { Divider } from '@/common/components/Divider/Divider.tsx'
+
+import HomeIcon from '@/assets/icons/home.svg?react'
+import LibraryMusicIcon from '@/assets/icons/library-music.svg?react'
+import AddCircleIcon from '@/assets/icons/add-circle.svg?react'
+import FileUploadIcon from '@/assets/icons/file-upload.svg?react'
+import MusicNoteIcon from '@/assets/icons/music-note.svg?react'
+import MusicVideoIcon from '@/assets/icons/music-video.svg?react'
+
+import s from './Navbar.module.scss'
 
 export const Navbar = () => {
     return (
@@ -13,19 +22,27 @@ export const Navbar = () => {
                         isActive ? `${s.activeLink}` : `${s.link}`
                     }
                 >
+                    <HomeIcon />
                     Home
                 </NavLink>
-                {/*<NavLink*/}
-                {/*    to={Path.Main}*/}
-                {/*    className={({ isActive }) =>*/}
-                {/*        isActive ? `${s.activeLink}` : `${s.link}`*/}
-                {/*    }*/}
-                {/*>*/}
-                {/*    Your Library*/}
-                {/*</NavLink> TODO make your library page*/}
+                <NavLink
+                    to={Path.Profile}
+                    className={({ isActive }) =>
+                        isActive ? `${s.activeLink}` : `${s.link}`
+                    }
+                >
+                    <LibraryMusicIcon />
+                    Your Library
+                </NavLink>
                 <Divider />
-                <button className={s.link}>Create Playlist</button>
-                <button className={s.link}>Upload Track</button>
+                <button className={s.link}>
+                    <AddCircleIcon />
+                    Create Playlist
+                </button>
+                <button className={s.link}>
+                    <FileUploadIcon />
+                    Upload Track
+                </button>
                 <Divider />
                 <NavLink
                     to={Path.Tracks}
@@ -33,6 +50,7 @@ export const Navbar = () => {
                         isActive ? `${s.activeLink}` : `${s.link}`
                     }
                 >
+                    <MusicNoteIcon />
                     All Tracks
                 </NavLink>
                 <NavLink
@@ -41,6 +59,7 @@ export const Navbar = () => {
                         isActive ? `${s.activeLink}` : `${s.link}`
                     }
                 >
+                    <MusicVideoIcon />
                     All Playlists
                 </NavLink>
             </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import vitePluginSvgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), vitePluginSvgr()],
     resolve: {
         alias: {
             '@/': `${path.resolve(__dirname, 'src')}/`,
