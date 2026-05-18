84075aadaecbc9d77aa399128fe2dbf88688a05c
talucre <talucreee@gmail.com>
2026-05-18 22:20:14 +0300

    feature: tracks infinite query

---
+++ src/features/tracks/api/trackApi.ts
@@ -0,0 +1,29 @@
+import { baseApi } from '@/app/api/baseApi.ts'
+import type { FetchTracksResponse } from './trackApi.types.ts'
+
+export const trackApi = baseApi.injectEndpoints({
+    endpoints: build => ({
+        fetchTracks: build.infiniteQuery<
+            FetchTracksResponse,
+            void,
+            string | null
+        >({
+            infiniteQueryOptions: {
+                initialPageParam: null,
+                getNextPageParam: lastPage => {
+                    return lastPage.meta.nextCursor
+                },
+            },
+            query: ({ pageParam }) => ({
+                url: 'playlists/tracks',
+                params: {
+                    cursor: pageParam,
+                    paginationType: 'cursor',
+                    pageSize: 5,
+                },
+            }),
+        }),
+    }),
+})
+
+export const { useFetchTracksInfiniteQuery } = trackApi
